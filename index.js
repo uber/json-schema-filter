1bcdbfebd6508471d2aa6fa5f883fbef87e3eb6a
Albert Wu <albertwu@uber.com>
2017-06-15 12:47:39 -0700

    refactor and add tests

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 module.exports = filterObjectOnSchema;
 
-
 function isObject(obj) {
     return obj === Object(obj);
 }
 
-function getType(typeThing) {
-    if (!Array.isArray(typeThing)) {
-        return typeThing;
+function getType(schemaType) {
+    if (!Array.isArray(schemaType)) {
+        return schemaType;
     }
 
     // the type is an array of types instead
     // grab the first non-null type as validator (e.g. ['object', 'null'])
-    var typeArray = typeThing;
+    var typeArray = schemaType;
     for (var i = 0; i < typeArray.length; i++) {
         var type = typeArray[i];
         if (type !== 'null') {
@@ -21,70 +20,43 @@ function getType(typeThing) {
     }
 }
 
-function filterObjectOnSchema(schema, doc){
+function filterObjectOnSchema(schema, doc) {
+    var result;  // returns the resulting filtered thing from this level; can be object, array, literal, ...
 
-    var results;
-    var type;
-
-    //console.log("DOC: ", JSON.stringify(doc, null, 2));
-    //console.log("SCH: ", JSON.stringify(schema, null, 2));
-
-    type = getType(schema.type);
-    if (type === 'object') {
-      results = {};   // holds this levels items
-
-      if (!schema.properties) {
+    // if the document is null/undefined, short-circuit and return it
+    if (doc === null || doc === undefined) {
         return doc;
-      }
-
-      if (!isObject(doc)) {
-        return doc;
-      }
-
-      // process properties  -  recursive
-      Object.keys(schema.properties).forEach(function(key){
-        if (doc[key] !== undefined){
-          var sp = schema.properties[key];
-          type = getType(sp.type);
-          if ( type === 'object'){
-
-            // check if property-less object (free-form)
-            if (sp.hasOwnProperty('properties')){
-              results[key] = filterObjectOnSchema(sp, doc[key]);
-            } else {
-              if (!isObject(doc[key])) {
-                results[key] = doc[key];
-              } else if (Object.keys(doc[key]).length > 0){
-                results[key] = doc[key];
-              } else {
-                results[key] = {};
-              }
-            }
-
-          }else if(type === 'array'){
-            if (doc[key]) results[key] = filterObjectOnSchema(sp, doc[key]);
-          }
-          else if(type === 'boolean' || type === 'number' || type === 'integer'){
-            if(doc[key] != null && typeof doc[key] != 'undefined') results[key] = doc[key];
-          }else{
-            if (doc[key] !== undefined && doc[key] !== null) results[key] = doc[key];
-          }
-        }
-      });
-
-    } else if (type === 'array'){
-      // arrays can hold objects or literals
-      if (schema.items.type === 'object' && Array.isArray(doc)) {
-        results = [];
-        doc.forEach(function (item) {
-          results.push(filterObjectOnSchema(schema.items, item));
-        });
-      } else {
-        results = doc;
-      }
     }
 
-    return results;
+    // otherwise, let's build the result based on casework
+    var type = getType(schema.type);
+    if (type === 'object' && isObject(doc) && schema.properties) {
+        result = {};  // holds filtered set of object key/values from this level
 
+        // process properties recursively
+        Object.keys(schema.properties).forEach(function(key) {
+            var child = doc[key];
+            var sp = schema.properties[key];
+
+            var filteredChild = filterObjectOnSchema(sp, child);
+
+            // filter out if the child is undefined
+            if (filteredChild === undefined) {
+                return;
+            }
+
+            // keep the child if it's defined properly or null
+            result[key] = filteredChild;
+        });
+    } else if (type === 'array' && Array.isArray(doc)) {
+        // check that the doc is also an array
+        result = [];
+        doc.forEach(function(item) {
+            result.push(filterObjectOnSchema(schema.items, item));
+        })
+    } else {  // literals, or if object/array schema def. vs real thing doesn't match
+        result = doc;
+    }
+
+    return result;
 }
-
